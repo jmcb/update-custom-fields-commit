c3609f286eec80d4d5f3d980b2206017b742a89e
Jonathan <tom@acss.net.au>
2011-02-11 11:33:22 +1000

    Refactor hooks.rb.
    Hopefully this actually brings it into line with how things are supposed
    to be done. Guess I should actually try learning Ruby before I try
    writing plugins for something? :)

--- lib/update-custom-fields-commit/hooks.rb
+++ lib/update-custom-fields-commit/hooks.rb
@@ -1,15 +1,21 @@
 module UpdateCustomFieldsOnCommit
-    class Hooks < Redmine::Hook::ModelListener
-        def model_changeset_scan_commit_for_issue_ids_pre_issue_update (context = { })
-            if context[:issue]
-                if context[:issue][:status].is_closed
-                    if context[:issue][:tracker].name == "Bug"
-                        context[:issue].custom_field_values[2] = "Done"
-                    elsif context[:issue][:tracker].name == "Feature"
-                        context[:issue].custom_field_values[3] = "Done"
+    module Hooks
+        class FieldsHook < Redmine::Hook::ViewListener
+
+            def model_changeset_scan_commit_for_issue_ids_pre_issue_update (context={ })
+                return unless context[:issue]
+
+                issue = context[:issue]
+
+                if issue.status.is_closed?
+                    if issue.tracker[:name] == "Bug"
+                        issue.custom_field_values[2] = "Done"
+                    elsif issue.tracker[:name] == "Feature"
+                        issue.custom_field_values[3] = "Done"
                     end
                 end
             end
+
         end
     end
 end
