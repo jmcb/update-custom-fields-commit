6048e31b0e5c719cacce09a08bbd3debe29cd972
Jonathan <tom@acss.net.au>
2011-02-11 12:40:17 +1000

    Let's confirm that we're actually doing something to the context.

--- lib/update-custom-fields-commit/hooks.rb
+++ lib/update-custom-fields-commit/hooks.rb
@@ -7,12 +7,8 @@ module UpdateCustomFieldsOnCommit
 
                 issue = context[:issue]
 
-                if issue.status.is_closed?
-                    if issue.tracker[:name] == "Bug"
-                        issue.custom_field_values[2] = "Done"
-                    elsif issue.tracker[:name] == "Feature"
-                        issue.custom_field_values[3] = "Done"
-                    end
+                if issue.tracker[:name] == "Bug":
+                    issue.done_ratio = 10
                 end
 
                 issue.save
