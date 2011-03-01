cb41c52cfed3f0f5d6ee40e2962a318ba4cc861e
Jonathan <jonathan@acss.net.au>
2011-03-02 09:57:41 +1000

    Include the "default" settings and links properly.
    Hopefully properly.

--- init.rb
+++ init.rb
@@ -8,6 +8,13 @@ Redmine::Plugin.register :update_custom_fields_on_commit do
   url 'http://github.com/jmcb/update-custom-fields-commit'
   author_url 'http://githu.com/jmcb'
   description 'Update specific custom fields, dependent on tracker, when closing an issue via commit.'
+  settings :default => { 'fields' => 
+                            {'tracker' => 0,
+                             'custom_field' => 0,
+                             'custom_value' => 0 },
+                         'menu' => 'Commit-updated custom fields.' },
+           :partial => 'settings/update_custom_fields_on_commit'
+
   version '0.1'
 
   requires_redmine :version_or_higher => '0.8.0'
